5ace10ffde48f5fc8922b3ddd0c0a455f6a95361
Andrew Favaloro <andrew.favaloro@gmail.com>
2021-12-04 13:33:44 -0500

    Why isn't commit --amend an option?

--- scripts.js
+++ scripts.js
@@ -183,7 +183,7 @@ class calendarElement {
             return '<button disabled>No Streaming Link Available</button>';
         }
         const streamingProvider = calendarAux.determineStreamingProvider(link)
-        const streamingClass = streamingProvider.replace(/ /g, "_").replace(/(!|+)/g,'')
+        const streamingClass = streamingProvider.replace(/ /g, "_").replace(/(!|\+)/g,'')
         const buttonText = this.buttonVerb() + (streamingProvider.length > 0 ? ` on ${streamingProvider}` : ``);
         return `
         <a href="${link}" target="_blank" rel="noopener noreferrer">
